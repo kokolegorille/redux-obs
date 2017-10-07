38e1d5b200a0290072fd9e76111919ea98701ceb
koko.le.gorille <koko.le.gorille@gmail.com>
2017-10-07 04:08:59 +0200

    Add basic epic

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,9 +1,11 @@
 import {Observable} from 'rxjs';
 import {combineEpics} from 'redux-observable';
 
+import * as types from '../actions/action_types';
+
 const loadStoriesEpic = action$ => (
-  action$
-    .do(action => console.log(action))
+  action$.ofType(types.LOAD_STORIES)
+    .do(action => console.log("From Epic : ", action))
     .ignoreElements()
 );
 
